a9b90ce6258dac805f2c87018f0b6763ef46b48b
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-04-15 00:34:38 +0200

    created chat for one topic

--- ReactApp/src/Components/ArticleComponent.js
+++ ReactApp/src/Components/ArticleComponent.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import SliderData from "../Components/SliderData.js";
-import img from "../images/3637164.jpg";
 import firebase from "firebase/app";
-import axios from "axios";
 
 const ArticleComponent = (props) => {
-  const [input, setInput] = useState("chemistry");
+  const [input, setInput] = useState('chemistry');
   const [articleIndex, setArticleIndex] = useState(0);
   const [articleTitle, setArticleTitle] = useState('');
   const [articleContent, setArticleContent] = useState('');
@@ -25,7 +23,7 @@ const ArticleComponent = (props) => {
       try{
         dataArticles = [...dataArticles, article];
         setArticles(dataArticles); // use the useState articles 
-        console.log(articles.length);
+        console.log("ARTICLESS" + articles);
         setArticleTitle(articles[articleIndex].data().title);
         setArticleContent(articles[articleIndex].data().content)
         setArticleImage(articles[articleIndex].data().img) 
@@ -35,7 +33,7 @@ const ArticleComponent = (props) => {
       }
        
     });
-    return dataArticles;
+    // return dataArticles;
   };
 
 
@@ -49,12 +47,14 @@ const ArticleComponent = (props) => {
   }
 
   const onIndexIncreaseHandler = () => {
+    if(articleIndex === 1) return
     setArticleIndex(articleIndex + 1);
-    console.log(articles)
+    // console.log(articles)
     // setArticleTitle(articles[articleIndex].data().title)
   }
 
   const onIndexDecreaseHandler = () => {
+    if(articleIndex === 0) return
     setArticleIndex(articleIndex - 1);
   }
 
@@ -64,38 +64,47 @@ const ArticleComponent = (props) => {
   //   }
   // })
 
-  return (
-    <div className="jumbotron vertical-center">
-      <div className="article-container">
-        <select
-          name="topic"
-          id="topic"
-          onChange={onChangeHandler}
-          value={input}
-        >
-          <option value="chemistry" >Chemistry</option>
-          <option value="biology">Biology</option>
-          <option value="geography">Geography</option>
-          <option value="history">History</option>
-        </select>
-        {SliderData.map((slide, index) => {
-          return (
-            <div className="card text-center" key={index}>
-              <div className="overflow">
-                <img src={articleImage} alt="article image" className="article-image" />
+  if(articles.length === 0) return <h1>Loading...</h1>
+
+    
+
+  else{
+    return (
+      <div className="jumbotron vertical-center">
+        <div className="article-container">
+          <select
+            name="topic"
+            id="topic"
+            onChange={onChangeHandler}
+            // value={input}
+          >
+            <option value="chemistry" >Chemistry</option>
+            <option value="biology">Biology</option>
+            <option value="geography">Geography</option>
+            <option value="history">History</option>
+          </select>
+          {SliderData.map((slide, index) => {
+            console.log("FIRST")
+            return (
+              <div className="card text-center" key={index}>
+                <div className="overflow">
+                  <img src={articleImage} alt="article image" className="article-image" />
+                </div>
+                <div className="card-body text-dark">
+                  <h4 className="card-title">{articleTitle}</h4>
+                  <p className="card-text text-secondary">{articleContent}</p>
+                </div>
               </div>
-              <div className="card-body text-dark">
-                <h4 className="card-title">{articleTitle}</h4>
-                <p className="card-text text-secondary">{articleContent}</p>
-              </div>
-            </div>
-          );
-        })}
-        <button id="goLeft" onClick={onIndexDecreaseHandler}>{`<`}</button>
-        <button id="goRight" onClick={onIndexIncreaseHandler}>{`>`}</button>
+            );
+          })}
+          <button id="goLeft" onClick={onIndexDecreaseHandler}>{`<`}</button>
+          <button id="goRight" onClick={onIndexIncreaseHandler}>{`>`}</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  
 };
 
 export default ArticleComponent;
--- ReactApp/src/Components/ChatComponent.js
+++ ReactApp/src/Components/ChatComponent.js
@@ -1,9 +1,56 @@
-
+import firebase from "firebase/app";
+import React, { useRef, useState } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import '../Chat.css';
 
 const ChatComponent = props =>{
+    const db = firebase.firestore();
+
+    const messagesRef = db.collection('messages').doc('biology').collection('chats')
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    function ChatMessage(props) {
+        const { text, uid } = props.message;
+
+        const messageClass = uid === sessionStorage.uid ? 'sent' : 'received';
+
+        return (
+        <div className={`message ${messageClass}`}>
+            <p>{text}</p>
+        </div>
+        )
+    }
+
+    const [messages] = useCollectionData(query, {idField: 'id'})
+
+    const [formValue, setFormValue] = useState('')
+
+    const sendMessage = async(e) => {
+        e.preventDefault();
+
+        const uid = sessionStorage.uid;
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid
+        })
+
+        setFormValue('')
+    }
+
     return (
     
-        <h1>dasd</h1>
+        <>
+        <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+        </div>
+
+        <form onSubmit={sendMessage} className='forms'>
+            <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+
+            <button type="submit" className='send'>Send</button>
+        </form>
+        </>
     );
 }
 

7d43638b76b257f802de16d51220e5f771eb4651
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-04-18 17:46:32 +0200

    article page fixed design issues, game page fixed grid system

--- ReactApp/src/Components/ArticleComponent.js
+++ ReactApp/src/Components/ArticleComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import SliderData from "../Components/SliderData.js";
 import firebase from "firebase/app";
 
@@ -45,73 +45,76 @@ const ArticleComponent = (props) => {
   };
 
   const onIndexIncreaseHandler = () => {
-    if(articleIndex === 1) return
+    if (articleIndex === 1) return;
     setArticleIndex(articleIndex + 1);
-    console.log(articles)
+    console.log(articles);
   };
 
   const onIndexDecreaseHandler = () => {
-    if(articleIndex === 0) return
+    if (articleIndex === 0) return;
     setArticleIndex(articleIndex - 1);
   };
 
-
   return (
-    <div  className="container">
-      <div className="row align-items-start">
-      <Link to="/menu">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="30"
-      fill="currentColor"
-      class="bi bi-arrow-left-circle-fill"
-      viewBox="0 0 16 16"
-    >
-      <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
-    </svg>
-  </Link>
-      </div>
-      
-     
-      <div className="row align-self-center">
-        <div className="col-lg-6 col-md-4 col-sm-2">
-        <select
-          name="topic"
-          id="topic"
-          onChange={onChangeHandler}
-          value={input}
-        >
-          <option value="chemistry">Chemistry</option>
-          <option value="biology">Biology</option>
-          <option value="geography">Geography</option>
-          <option value="history">History</option>
-        </select>
+    <div className="container">
+      <div className="row my-row justify-content-between">
+        <div className="col-lg-1 col-md-2 col-sm-1 col-xs-2">
+          <Link to="/menu">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-left-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+            </svg>
+          </Link>
         </div>
-        <div className="row card text-center">
-        {SliderData.map((slide, index) => {
-          return (
-            <>
-              <div className="col-lg-6 col-md-4 col-sm-2">
-                <img
-                  src={articleImage}
-                  alt="article image"
-                  className="article-image"
-                />
-              </div>
-              <div className="col-12 card-body text-dark">
-                <h4 className="card-title">{articleTitle}</h4>
-                <p className="card-text text-secondary">{articleContent}</p>
-              </div>
-            </>
-          );
-        })}
+
+        <div className="col-lg-7 col-md-6 col-sm-4 col-xs-6">
+          <select
+            name="topic"
+            id="topic"
+            onChange={onChangeHandler}
+            value={input}
+          >
+            <option value="chemistry">Chemistry</option>
+            <option value="biology">Biology</option>
+            <option value="geography">Geography</option>
+            <option value="history">History</option>
+          </select>
+        </div>
+      </div>
+
+      
+        <div className="card text-center">
+          {SliderData.map((slide, index) => {
+            return (
+              <>
+                <div className="row align-self-center">
+                  <img
+                    src={articleImage}
+                    alt="article image"
+                    className="article-image"
+                  />
+                </div>
+                <div className="row card-row">
+                  <div className="col my-col">
+                  <h4 className="card-title">{articleTitle}</h4>
+                  <p className="card-text text-secondary">{articleContent}</p>
+                  </div>
+                  
+                </div>
+              </>
+            );
+          })}
         </div>
         <button id="goLeft" onClick={onIndexDecreaseHandler}>{`<`}</button>
         <button id="goRight" onClick={onIndexIncreaseHandler}>{`>`}</button>
-      </div>
+      
     </div>
-    
   );
 };
 
--- ReactApp/src/Components/GameComponent.js
+++ ReactApp/src/Components/GameComponent.js
@@ -1,231 +1,245 @@
-import {Link} from "react-router-dom"
-import Questions from "../Services/Questions"
-import firebase from "firebase/app"
-import firestore from "firebase/firestore"
-import { useEffect, useState } from "react"
-import * as arrayFunctions from "../Services/shuffle.js"
-import LocateUser from "../Services/LocateUser.js"
+import { Link } from "react-router-dom";
+import Questions from "../Services/Questions";
+import firebase from "firebase/app";
+import firestore from "firebase/firestore";
+import { useEffect, useState } from "react";
+import * as arrayFunctions from "../Services/shuffle.js";
+import LocateUser from "../Services/LocateUser.js";
 import { withRouter } from "react-router";
 
+const GameComponent = (props) => {
+  const [lastQuestion, setLastQuestion] = useState({
+    question: "",
+    answers: ["", "", "", ""],
+  });
+  const [lastPQuestion, setLastPQuestion] = useState({
+    question: "",
+    answers: ["", "", "", ""],
+  });
+  const [answers, setAnswers] = useState(["", "", "", ""]);
+  const [nextQuestion, setNextQuestion] = useState(true);
+  const [questionType, setQuestionType] = useState(["topic", "region"]);
+  const db = firebase.firestore();
+  const [userScore, setUserScore] = useState("");
+  const [showQuestion, setShowQuestion] = useState("topic");
 
+  const getNext = async () => {};
 
-const GameComponent = props => {
-    const [lastQuestion, setLastQuestion] = useState({
-        question: "",
-        answers: ["", "", "", ""]
-    })
-    const [lastPQuestion, setLastPQuestion] = useState({
-        question: "",
-        answers: ["", "", "", ""]
-    })
-    const [answers, setAnswers] = useState(["", "", "", ""])
-    const [nextQuestion, setNextQuestion] = useState(true)
-    const [questionType, setQuestionType] = useState(["topic", "region"])
-    const db = firebase.firestore()
-    const [userScore, setUserScore] = useState("")
-    const [showQuestion,setShowQuestion] = useState("topic")
-
-
-    const getNext = async () => {
-
+  useEffect(async () => {
+    // setUserScore(sessionStorage.getItem())
+    if (lastPQuestion.question === "" && lastQuestion === "") {
+      setNextQuestion(!nextQuestion);
     }
+    const userProvince = await LocateUser.getUserProvince();
+    const userProvinceLC = userProvince.toLowerCase().replace(/\s/g, "");
+    setQuestionType(arrayFunctions.shuffle(questionType));
+    if (questionType[0] === "topic") {
+      if (lastQuestion.question == "") {
+        console.log("PRAZNO");
+        const first = await db
+          .collection("questions")
+          .doc("topics")
+          .collection("geography")
+          .orderBy("createdAt", "asc")
+          .limit(1);
 
+        const snapshot = await first.get();
 
-    useEffect(async () => {
-       // setUserScore(sessionStorage.getItem())
-        if (lastPQuestion.question === "" && lastQuestion === "") {
-            setNextQuestion(!nextQuestion)
+        // Get the last document
+        const last = snapshot.docs[snapshot.docs.length - 1];
+        setLastQuestion(last.data());
+        setAnswers(await arrayFunctions.shuffle(last.data().answers));
+        setShowQuestion("topic");
+        console.log(answers);
+        //setNextQuestion(false)
+        //console.log(lastQuestion)
+      }
+      // Construct a new query starting at this document.
+      // Note: this will not have the desired effect if multiple
+      // cities have the exact same population value.
+      else {
+        const nextt = db
+          .collection("questions")
+          .doc("topics")
+          .collection("geography")
+          .orderBy("createdAt", "asc")
+          .startAfter(lastQuestion.createdAt)
+          .limit(1);
+        const nextSnapshot = await nextt.get();
+        const lastt = nextSnapshot.docs[nextSnapshot.docs.length - 1];
+        try {
+          setLastQuestion(lastt.data());
+          setAnswers(await arrayFunctions.shuffle(lastt.data().answers));
+          setShowQuestion("topic");
+          // setNextQuestion(false)
+        } catch (err) {
+          console.log("no more questions");
         }
-        const userProvince = await LocateUser.getUserProvince()
-        const userProvinceLC = userProvince.toLowerCase().replace(/\s/g, '')
-        setQuestionType(arrayFunctions.shuffle(questionType))
-        if (questionType[0] === "topic") {
+      }
+    } else {
+      if (lastPQuestion.question == "") {
+        console.log("PRAZNO");
+        const first = await db
+          .collection("questions")
+          .doc(userProvinceLC)
+          .collection("questions")
+          .orderBy("createdAt", "asc")
+          .limit(1);
 
-            if (lastQuestion.question == "") {
-                console.log("PRAZNO")
-                const first = await db
-                    .collection("questions")
-                    .doc("topics")
-                    .collection("geography")
-                    .orderBy('createdAt', "asc")
-                    .limit(1);
+        const snapshot = await first.get();
 
-                const snapshot = await first.get();
-
-                // Get the last document
-                const last = snapshot.docs[snapshot.docs.length - 1];
-                setLastQuestion(last.data())
-                setAnswers(await arrayFunctions.shuffle(last.data().answers))
-                setShowQuestion("topic")
-                console.log(answers)
-                //setNextQuestion(false)
-                //console.log(lastQuestion)
-            }
-            // Construct a new query starting at this document.
-            // Note: this will not have the desired effect if multiple
-            // cities have the exact same population value.
-            else {
-                const nextt = db
-                    .collection("questions")
-                    .doc("topics")
-                    .collection("geography")
-                    .orderBy('createdAt', "asc")
-                    .startAfter(lastQuestion.createdAt)
-                    .limit(1);
-                const nextSnapshot = await nextt.get();
-                const lastt = nextSnapshot.docs[nextSnapshot.docs.length - 1];
-                try {
-                    setLastQuestion(lastt.data())
-                    setAnswers(await arrayFunctions.shuffle(lastt.data().answers))
-                    setShowQuestion("topic")
-                    // setNextQuestion(false)
-                }
-                catch (err) {
-                    console.log("no more questions")
-                }
-
-
-
-            }
+        // Get the last document
+        const last = snapshot.docs[snapshot.docs.length - 1];
+        setLastPQuestion(last.data());
+        setAnswers(await arrayFunctions.shuffle(last.data().answers));
+        setShowQuestion("province");
+        console.log(answers);
+        //setNextQuestion(false)
+        //console.log(lastQuestion)
+      }
+      // Construct a new query starting at this document.
+      // Note: this will not have the desired effect if multiple
+      // cities have the exact same population value.
+      else {
+        const nextt = db
+          .collection("questions")
+          .doc(userProvinceLC)
+          .collection("questions")
+          .orderBy("createdAt", "asc")
+          .startAfter(lastPQuestion.createdAt)
+          .limit(1);
+        const nextSnapshot = await nextt.get();
+        const lastt = nextSnapshot.docs[nextSnapshot.docs.length - 1];
+        try {
+          setLastPQuestion(lastt.data());
+          setAnswers(await arrayFunctions.shuffle(lastt.data().answers));
+          setShowQuestion("province");
+          //setNextQuestion(false)
+        } catch (err) {
+          console.log("no more questions");
         }
-        else {
-
-            if (lastPQuestion.question == "") {
-                console.log("PRAZNO")
-                const first = await db
-                    .collection("questions")
-                    .doc(userProvinceLC)
-                    .collection("questions")
-                    .orderBy('createdAt', "asc")
-                    .limit(1);
-
-                const snapshot = await first.get();
-
-                // Get the last document
-                const last = snapshot.docs[snapshot.docs.length - 1];
-                setLastPQuestion(last.data())
-                setAnswers(await arrayFunctions.shuffle(last.data().answers))
-                setShowQuestion("province")
-                console.log(answers)
-                //setNextQuestion(false)
-                //console.log(lastQuestion)
-            }
-            // Construct a new query starting at this document.
-            // Note: this will not have the desired effect if multiple
-            // cities have the exact same population value.
-            else {
-                const nextt = db
-                    .collection("questions")
-                    .doc(userProvinceLC)
-                    .collection("questions")
-                    .orderBy('createdAt', "asc")
-                    .startAfter(lastPQuestion.createdAt)
-                    .limit(1);
-                const nextSnapshot = await nextt.get();
-                const lastt = nextSnapshot.docs[nextSnapshot.docs.length - 1];
-                try {
-                    setLastPQuestion(lastt.data())
-                    setAnswers(await arrayFunctions.shuffle(lastt.data().answers))
-                    setShowQuestion("province")
-                    //setNextQuestion(false)
-                }
-                catch (err) {
-                    console.log("no more questions")
-                }
-
-
-            }
-
-        }
-        console.log(lastQuestion)
-    }, [nextQuestion]);
-
-
-
-
-
-    const handleClick = async e => {
-        e.preventDefault()
-        const userRef = db.collection('users').doc(sessionStorage.getItem("uid"));
-        console.log(questionType)
-        if (questionType[0] === "topic") {
-
-            if (e.target.innerHTML === lastQuestion.answers[0]) {
-                const currentScore = parseInt(sessionStorage.getItem("score"))
-                const newScore = currentScore + lastQuestion.score
-                const res = await userRef.update({ score: newScore });
-                sessionStorage.setItem("score", newScore)
-                console.log(sessionStorage.getItem("score"))
-
-
-            }
-            else {
-                const currentScore = parseInt(sessionStorage.getItem("score"))
-                const newScore = currentScore - (lastQuestion.score / 2)
-                const res = await userRef.update({ score: newScore });
-                sessionStorage.setItem("score", newScore)
-                 console.log(sessionStorage.getItem("score"))
-            }
-        }
-        else {
-            if (e.target.innerHTML === lastPQuestion.answers[0]) {
-                const currentScore = parseInt(sessionStorage.getItem("score"))
-                console.log(currentScore)
-                const newScore = currentScore + lastPQuestion.score
-                const res = await userRef.update({ score: newScore });
-                sessionStorage.setItem("score", newScore)
-                console.log(sessionStorage.getItem("score"))
-
-            }
-            else {
-                const currentScore = parseInt(sessionStorage.getItem("score"))
-                console.log(currentScore)
-                const newScore = currentScore - (lastPQuestion.score / 2)
-                console.log(newScore)
-                const res = await userRef.update({ score: newScore });
-                sessionStorage.setItem("score", newScore)
-                console.log(sessionStorage.getItem("score"))
-            }
-        }
-        console.log(nextQuestion)
-        setNextQuestion(!nextQuestion)
-        console.log(nextQuestion)
-       
+      }
     }
+    console.log(lastQuestion);
+  }, [nextQuestion]);
 
-   const  goToMenu = (e) =>{
-        props.history.push("/menu")
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const userRef = db.collection("users").doc(sessionStorage.getItem("uid"));
+    console.log(questionType);
+    if (questionType[0] === "topic") {
+      if (e.target.innerHTML === lastQuestion.answers[0]) {
+        const currentScore = parseInt(sessionStorage.getItem("score"));
+        const newScore = currentScore + lastQuestion.score;
+        const res = await userRef.update({ score: newScore });
+        sessionStorage.setItem("score", newScore);
+        console.log(sessionStorage.getItem("score"));
+      } else {
+        const currentScore = parseInt(sessionStorage.getItem("score"));
+        const newScore = currentScore - lastQuestion.score / 2;
+        const res = await userRef.update({ score: newScore });
+        sessionStorage.setItem("score", newScore);
+        console.log(sessionStorage.getItem("score"));
+      }
+    } else {
+      if (e.target.innerHTML === lastPQuestion.answers[0]) {
+        const currentScore = parseInt(sessionStorage.getItem("score"));
+        console.log(currentScore);
+        const newScore = currentScore + lastPQuestion.score;
+        const res = await userRef.update({ score: newScore });
+        sessionStorage.setItem("score", newScore);
+        console.log(sessionStorage.getItem("score"));
+      } else {
+        const currentScore = parseInt(sessionStorage.getItem("score"));
+        console.log(currentScore);
+        const newScore = currentScore - lastPQuestion.score / 2;
+        console.log(newScore);
+        const res = await userRef.update({ score: newScore });
+        sessionStorage.setItem("score", newScore);
+        console.log(sessionStorage.getItem("score"));
+      }
     }
-    return (
-        <div>
-            <Link to="/menu">
-              <svg xmlns="http://www.w3.org/2000/svg" onClick={goToMenu} width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+    console.log(nextQuestion);
+    setNextQuestion(!nextQuestion);
+    console.log(nextQuestion);
+  };
+
+  const goToMenu = (e) => {
+    props.history.push("/menu");
+  };
+  return (
+    <div className="container">
+      <div className="row score-row justify-content-between">
+        <div className="col-4">
+          <Link to="/menu">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={goToMenu}
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-left-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
             </svg>
-            </Link>
-
-            <p className = "score">Your score is: {sessionStorage.getItem("score")}</p>
-            <div class="container-fluid ">
-                <div class="row justify-content-center">
-                    <h2 class="sign">{showQuestion === "province" ? lastPQuestion.question : lastQuestion.question}</h2>
-                </div>
-                <div class="row justify-content-center">
-                    <button type="button" className="btn-primary btn-lg btn" onClick={handleClick}>{answers[0]}</button>
-                </div>
-                <div class="row justify-content-center">
-                    <button type="button" className="btn-primary btn-lg btn" onClick={handleClick}>{answers[1]}</button>
-                </div>
-                <div class="row justify-content-center">
-                    <button type="button" className="btn-primary btn-lg btn" onClick={handleClick}>{answers[2]}</button>
-                </div>
-                <div class="row justify-content-center">
-                    <button type="button" className="btn-primary btn-lg btn" onClick={handleClick}>{answers[3]}</button>
-                </div>
-            </div>
-          
+          </Link>
         </div>
 
-    )
-}
+        <div className="col-4">
+          <p className="score">
+            Your score is: {sessionStorage.getItem("score")}
+          </p>
+        </div>
+      </div>
+
+      
+        <div class="row justify-content-center">
+          <h2 class="sign">
+            {showQuestion === "province"
+              ? lastPQuestion.question
+              : lastQuestion.question}
+          </h2>
+        </div>
+        <div class="row btn-row justify-content-center">
+          <button
+            type="button"
+            className="btn-primary btn-lg btn"
+            onClick={handleClick}
+          >
+            {answers[0]}
+          </button>
+        </div>
+        <div class="row btn-row justify-content-center">
+          <button
+            type="button"
+            className="btn-primary btn-lg btn"
+            onClick={handleClick}
+          >
+            {answers[1]}
+          </button>
+        </div>
+        <div class="row btn-row justify-content-center">
+          <button
+            type="button"
+            className="btn-primary btn-lg btn"
+            onClick={handleClick}
+          >
+            {answers[2]}
+          </button>
+        </div>
+        <div class="row btn-row justify-content-center">
+          <button
+            type="button"
+            className="btn-primary btn-lg btn"
+            onClick={handleClick}
+          >
+            {answers[3]}
+          </button>
+        
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(GameComponent);
